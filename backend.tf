faaf5e54445e5e49aedba33787e7e958672a281a
Sai Kaushik Mudiki <mudiki08@gmail.com>
2021-05-07 09:58:48 -0400

    Updated the state bucket region

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
    backend "s3" {
         bucket = "eks-infra-mcx"
         key = "state/eks-cluster"
-        region = "us-east-1"
+        region = "us-west-2"
         encrypt = "true"
         workspace_key_prefix = "terraform"
     }
